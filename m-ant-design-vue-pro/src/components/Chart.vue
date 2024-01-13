f0194ac3dd1a1ecbba11e682ff26deca24674858
LvGJ <lvgj1998@gmail.com>
2024-01-14 01:03:08 +0800

    feat: 0.0.8 echart demo

---
+++ m-ant-design-vue-pro/src/components/Chart.vue
@@ -0,0 +1,52 @@
+<template>
+  <div ref="chartDom" style="height: 400px"></div>
+</template>
+
+<script>
+import echarts from "echarts";
+import debounce from "lodash/debounce";
+import { addListener, removeListener } from "resize-detector";
+export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  created() {
+    this.resize = debounce(this.resize, 300);
+  },
+  watch: {
+    option(val) {
+      this.myChart.setOption(val);
+    },
+    // option: {
+    //   handler(val) {
+    //     this.myChart.setOption(val);
+    //   },
+    //   deep: true,
+    // },
+  },
+  mounted() {
+    this.renderChart();
+    addListener(this.$refs.chartDom, this.resize);
+  },
+  beforeDestroy() {
+    removeListener(this.$refs.chartDom, this.resize);
+    this.myChart.dispose();
+    this.myChart = null;
+  },
+  methods: {
+    resize() {
+      console.log("resize");
+      this.myChart.resize();
+    },
+    renderChart() {
+      this.myChart = echarts.init(this.$refs.chartDom);
+      this.myChart.setOption(this.option);
+    },
+  },
+};
+</script>
+
+<style></style>
